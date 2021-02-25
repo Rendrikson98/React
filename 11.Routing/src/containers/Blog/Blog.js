f0ebe007e563cf7f70f1334938a5f38f1aa68f4f
Rendrikson Oliveira <40209027+Rendrikson98@users.noreply.github.com>
2021-02-25 16:42:57 -0300

    starting the section 11, watched up to lecture 14

---
+++ 11.Routing/src/containers/Blog/Blog.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+// import axios from 'axios';
+//import axios from '../../axios';
+import { Route, Link } from 'react-router-dom';
+
+import './Blog.css';
+import Posts from './Posts/Posts';
+import NewPost from './NewPost/NewPost';
+
+class Blog extends Component {
+    render() {
+        return (
+            <div className='Blog'>
+                <header >
+                    <nav >
+                        <ul>
+                            <li><Link to='/'>Home</Link></li>
+                            <li><Link to={{
+                                pathname: '/newPost',
+                                hash: '#submit',
+                                search: '?quick-submit=true'
+                            }}>New Post</Link></li>
+                        </ul>
+                    </nav>
+                </header>
+                <Route path='/' exact component={Posts} />
+                <Route path='/newPost' component={NewPost} />
+            </div>
+        );
+    }
+}
+
+export default Blog;
