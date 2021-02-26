a49e24c74b0e1696257904a41ee4324db301b4a1
Rendrikson Oliveira <40209027+Rendrikson98@users.noreply.github.com>
2021-02-26 16:59:52 -0300

    watched up to lecture 18 at section 11

--- 11.Routing/src/containers/Blog/Blog.js
+++ 11.Routing/src/containers/Blog/Blog.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 // import axios from 'axios';
 //import axios from '../../axios';
-import { Route, Link } from 'react-router-dom';
+import { Route, NavLink, Link } from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
+import Post from './FullPost/FullPost';
 
 class Blog extends Component {
     render() {
@@ -14,17 +15,25 @@ class Blog extends Component {
                 <header >
                     <nav >
                         <ul>
-                            <li><Link to='/'>Home</Link></li>
-                            <li><Link to={{
-                                pathname: '/newPost',
-                                hash: '#submit',
-                                search: '?quick-submit=true'
-                            }}>New Post</Link></li>
+                            <li>
+                                <NavLink
+                                    exact
+                                    to='/'
+                                >Home</NavLink></li>
+                            <li>
+                                <NavLink
+                                    to={{
+                                        pathname: '/newPost',
+                                        hash: '#submit',
+                                        search: '?quick-submit=true'
+                                    }}
+                                >New Post</NavLink></li>
                         </ul>
                     </nav>
                 </header>
                 <Route path='/' exact component={Posts} />
                 <Route path='/newPost' component={NewPost} />
+                <Route path='/:id' component={Post} />
             </div>
         );
     }
